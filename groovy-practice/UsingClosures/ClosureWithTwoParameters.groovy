67aba8ffdd77e280a1a4f994737921db7bc569a9
rofik28 <rofik28@users.noreply.github.com>
2015-01-06 15:41:13 +0600

    Created by Coding Ground Client

---
+++ groovy-practice/UsingClosures/ClosureWithTwoParameters.groovy
@@ -0,0 +1,10 @@
+def tellFortune(closure) {
+closure new Date("09/20/2012"), "Your day is filled with ceremony"
+}
+tellFortune() { date, fortune ->
+println "Fortune for ${date} is '${fortune}'"
+}
+
+tellFortune() { Date date, fortune ->
+println "Fortune for ${date} is '${fortune}'"
+}

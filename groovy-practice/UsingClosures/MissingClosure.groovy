c8f1c54eb69554b4ee003a66c834710977efa6ea
Mohammad Rafiqul Islam <rofik28@users.noreply.github.com>
2015-01-06 17:35:02 +0600

    Created by Coding Ground Client

---
+++ groovy-practice/UsingClosures/MissingClosure.groovy
@@ -0,0 +1,9 @@
+def doSomeThing(closure) {
+if (closure) {
+closure()
+} else {
+println "Using default implementation"
+}
+}
+doSomeThing() { println "Use specialized implementation" }
+doSomeThing()
